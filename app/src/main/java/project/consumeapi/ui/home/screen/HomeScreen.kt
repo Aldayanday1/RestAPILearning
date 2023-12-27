7d54567c3c90f42db4ac2b9b11fd0dbe01506a42
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-27 10:46:00 +0700

    Add val of scrollBehavior in HomeScreen

--- app/src/main/java/project/consumeapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/project/consumeapi/ui/home/screen/HomeScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -52,6 +53,8 @@ fun HomeScreen(
     onDetailClick: (Int) -> Unit = {},
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
 
 }
 
