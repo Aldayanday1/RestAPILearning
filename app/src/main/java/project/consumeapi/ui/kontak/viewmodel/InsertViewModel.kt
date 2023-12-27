1a09bb22edbab2fc6d07c97beb41552f661bae1e
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-27 17:10:16 +0700

    Add fun Kontak.toInsertUiEvent()

--- app/src/main/java/project/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/project/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -51,6 +51,12 @@ fun Kontak.toUiStateKontak(): InsertUiState = InsertUiState(
     insertUiEvent = toInsertUiEvent(),
 )
 
+fun Kontak.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    id = id,
+    nama = nama,
+    email = alamat,
+    nohp = telpon
+)
 
 
 
