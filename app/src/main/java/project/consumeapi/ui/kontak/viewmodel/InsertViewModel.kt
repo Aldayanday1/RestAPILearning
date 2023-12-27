9f3cd7610d277005af3983ff477bbc930cc1d207
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-27 13:25:35 +0700

    Create a new file InsertViewModel with the class in it

---
+++ app/src/main/java/project/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -0,0 +1,8 @@
+package project.consumeapi.ui.kontak.viewmodel
+
+import androidx.lifecycle.ViewModel
+import project.consumeapi.repository.KontakRepository
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel(){
+
+}
