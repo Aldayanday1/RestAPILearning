ebb4dccfe7a2806fd153bd7c2406a015d70668d9
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-12-27 15:25:05 +0700

    Add var insertKontakState in InsertViewModel

--- app/src/main/java/project/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/project/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -1,8 +1,13 @@
 package project.consumeapi.ui.kontak.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import project.consumeapi.repository.KontakRepository
 
 class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel(){
+
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
 
 }
